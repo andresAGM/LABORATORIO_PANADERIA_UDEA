7419e1558b4bb35d7fee6cfbada35f2ec340b443
Andres Garcia <alejogarciamorales@gmail.com>
2026-05-23 17:04:36 -0500

    CONEXIÓN A BASE DE DATOS

---
+++ src/main/java/org/panaderia/laboratorio_panaderia/LaboratorioPanaderiaApplication.java
@@ -0,0 +1,13 @@
+package org.panaderia.laboratorio_panaderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioPanaderiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratorioPanaderiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/panaderia/laboratorio_panaderia/LaboratorioPanaderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.panaderia.laboratorio_panaderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioPanaderiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
